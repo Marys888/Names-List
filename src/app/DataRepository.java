e0753189b4b5535a43306af0a31089679132e86c
Marys888 <severovamaha1999@gmail.com>
2025-05-22 12:57:23 +0200

    Add data repository implementation

---
+++ src/app/DataRepository.java
@@ -0,0 +1,11 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataRepository {
+    public List<String> getData() {
+        List<String> namesList = new ArrayList<>();
+        return namesList;
+    }
+}
